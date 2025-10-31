f449f94966fc0b2ee818b10f3250f19f2b44c691
Mark Lu <yimminglu@gmail.com>
2025-10-31 03:11:04 -0400

    removed solidqueue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,7 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
+  config.active_job.queue_adapter = :inline
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
