d9c944f90bd0caaa98f1ba3e00c845a5f2338f93
Mark Lu <yimminglu@gmail.com>
2025-10-31 02:44:50 -0400

    Finalize SolidQueue jobs schema

---
+++ db/migrate/20251031064259_finalize_solid_queue_jobs_schema.rb
@@ -0,0 +1,14 @@
+class FinalizeSolidQueueJobsSchema < ActiveRecord::Migration[8.0]
+  def change
+    # Rename old column if needed
+    if column_exists?(:solid_queue_jobs, :job_class) && !column_exists?(:solid_queue_jobs, :class_name)
+      rename_column :solid_queue_jobs, :job_class, :class_name
+    end
+
+    # Add missing columns safely (won’t duplicate)
+    add_column :solid_queue_jobs, :active_job_id, :string unless column_exists?(:solid_queue_jobs, :active_job_id)
+    add_column :solid_queue_jobs, :priority, :integer, default: 0, null: false unless column_exists?(:solid_queue_jobs, :priority)
+    add_column :solid_queue_jobs, :concurrency_key, :string unless column_exists?(:solid_queue_jobs, :concurrency_key)
+    add_column :solid_queue_jobs, :expires_at, :datetime unless column_exists?(:solid_queue_jobs, :expires_at)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_31_063533) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_31_064259) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -96,7 +96,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_31_063533) do
 
   create_table "solid_queue_jobs", force: :cascade do |t|
     t.string "queue_name", null: false
-    t.string "job_class", null: false
+    t.string "class_name", null: false
     t.text "arguments", default: "{}"
     t.datetime "scheduled_at"
     t.datetime "finished_at"
